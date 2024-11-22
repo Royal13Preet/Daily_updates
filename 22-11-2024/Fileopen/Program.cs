57b5e7cf0d3752b3700528d1ba31d3b317d09d02
Preethu P S <pspreethu05@gmail.com>
2024-11-22 14:17:54 +0530

    My second Commit

---
+++ 22-11-2024/Fileopen/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fileopen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fp = @"C:\Users\preethu\Word.txt";
+            FileStream ab = new FileStream(fp, FileMode.Create);
+            ab.Close();
+            Console.WriteLine("Hello world!!");
+        }
+    }
+}
