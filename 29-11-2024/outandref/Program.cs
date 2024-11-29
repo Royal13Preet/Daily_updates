a9b4ab824fd18245a7cd1eac8d20b588e670a5f2
Preethu P S <pspreethu05@gmail.com>
2024-11-29 18:44:16 +0530

    my sixth commit

---
+++ 29-11-2024/outandref/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace outandref
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int g;
+            sum(out g);
+            Console.WriteLine("The some of ; {0}", g);
+        }
+        
+        public static void sum(out int g)
+        {
+            g = 20;
+            g += g;
+        }
+
+
+        
+    }
+}
+
+      
+
+
