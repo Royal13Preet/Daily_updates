a1cab5a63fdde849f97fdbbdda3c7229925bc9da
Preethu P S <pspreethu05@gmail.com>
2024-12-02 18:48:28 +0530

    Entity Framework

---
+++ 2-12-2024/APIwithEntityFrameworkcore/APIwithEntityFrameworkcore/DBModels/Course.cs
@@ -0,0 +1,13 @@
+﻿namespace APIwithEntityFrameworkcore.DBModels
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string description { get; set; }
+
+        //navigating propery
+        // a course can have many students 1:M
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ 2-12-2024/APIwithEntityFrameworkcore/APIwithEntityFrameworkcore/DBModels/Student.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIwithEntityFrameworkcore.DBModels
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+
+        public string StudentAddress { get; set; }
+
+        public string StudentEmail { get; set; }
+
+        public DateTime StudebtDoB { get; set; }
+        public string StudentGender { get; set; }
+
+        public DateTime dateAdd { get; set; }
+       
+        //Navigating property
+        //a student can have only one course 1:1
+
+        public int courseId;
+        public Course Course { get; set; }
+
+
+    }
+}
