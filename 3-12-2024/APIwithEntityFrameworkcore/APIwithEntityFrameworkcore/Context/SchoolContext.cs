bce0820e0c732903de02aa4916e995435babaf1c
Preethu P S <pspreethu05@gmail.com>
2024-12-03 18:10:13 +0530

    databases

---
+++ 3-12-2024/APIwithEntityFrameworkcore/APIwithEntityFrameworkcore/Context/SchoolContext.cs
@@ -0,0 +1,38 @@
+﻿using APIwithEntityFrameworkcore.DBModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIwithEntityFrameworkcore.Context
+{
+    public class SchoolContext : DbContext
+    {
+        IConfiguration Configuration { get; }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Course { get; set; }
+
+        public DbSet<Teacher> Teachers { get; set; }
+        public SchoolContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+        }
+
+       protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            //optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                .HasOne<Course>(s => s.Course)
+                .WithMany(c => c.Students)
+                .HasForeignKey(s => s.courseId);
+            
+            // cong=figure default values for student
+            modelBuilder.Entity<Student>()
+                .Property(s => s.dateAdd)
+                .HasDefaultValueSql("getdate()");
+        }
+    }
+}
---
+++ 3-12-2024/APIwithEntityFrameworkcore/APIwithEntityFrameworkcore/Migrations/20241202120141_Init.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIwithEntityFrameworkcore.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Course",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    phone = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudentAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudentEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudebtDoB = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StudentGender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                    table.ForeignKey(
+                        name: "FK_Students_Course_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Course",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_CourseId",
+                table: "Students",
+                column: "CourseId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+
+            migrationBuilder.DropTable(
+                name: "Course");
+        }
+    }
+}
