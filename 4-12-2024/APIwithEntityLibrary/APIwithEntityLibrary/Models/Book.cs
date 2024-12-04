2d1f9f3c37cec039f2e84c454cc0e1931cbab7df
Preethu P S <pspreethu05@gmail.com>
2024-12-04 18:22:32 +0530

    fluent api

---
+++ 4-12-2024/APIwithEntityLibrary/APIwithEntityLibrary/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace APIwithEntityLibrary.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+
+        public string Title { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author author { get; set; }
+
+        public ICollection<BookCategory> BookCategories { get; set; }
+    }
+}
